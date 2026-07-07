cbd8d01f1dc93be20296dbf64b67f4ec62ae8636
Firman <firman@nexl.cloud>
2026-07-07 17:14:37 +1000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Firman's darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
+    darwinConfigurations."Firmans-MacBook" = nix-darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
